ed9390c53c23b3e94a3f1719007cbb27bc115e61
José Hydeo Mesquita Watase <peo_jwatase@uolinc.com>
2022-06-10 10:15:20 -0300

    NextJS deploying on Vercel.

--- pages/faq.js
+++ pages/faq.js
@@ -1,45 +1,24 @@
-import Link from "../src/components/Link/"
-import PageTitle from "../src/components/PageTitle"
+// faq.js
+import FAQScreen from '../src/screens/FAQScreen';
 
+export default FAQScreen;
 
-
-export async function getStaticProps(context) {
-    const FAQ_API_URL = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json"
-
-    const faqs = await fetch(FAQ_API_URL)
-        .then((serverResponse) => {
-            return serverResponse.json()
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
         })
-        .then((response) => {
-            return response
-        })
+        .then((resposta) => {
+            return resposta;
+        });
 
     return {
-        props: { faqs }, // will be passed to the page component as props
-    }
-}
-
-export default function Faq({ faqs }) {
-    console.log(faqs)
-    return (
-        <div>
-            <PageTitle>Faq Page</PageTitle>
-            <h1>Faq Page</h1>
-            <Link href="/" passHref>
-                Return
-            </Link>
-            <ul>
-                {faqs.map(({ answer, question }) => {
-                    return (
-                        <li key={question}>
-                            <article>
-                                <h2>{question}</h2>
-                                <p>{answer}</p>
-                            </article>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
-}
+        props: {
+            qualquercoisa: 'que eu passar aqui',
+            faq,
+        },
+    };
+}  
